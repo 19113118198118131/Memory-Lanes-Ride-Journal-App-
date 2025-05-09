eb8110078269fb2b3cd24822b5a3ff9b0c441e6a
19113118198118131 <samar.sharma@live.com>
2025-05-09 23:10:33 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // script.js
 
-console.log('script.js loaded');
-window.updatePlayback = null;
-
 // 1) Import & init Supabase
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 const SUPABASE_URL     = 'https://vodujxiwkpxaxaqnwkdd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZHVqeGl3a3B4YXhhcW53a2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NTgwOTQsImV4cCI6MjA2MjMzNDA5NH0.k4NeZ3dgqe1QQeXmkmgThp-X_PwOHPHLAQErg3hrPok';
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.supabase = supabase;
+
+console.log('script.js loaded');
+window.updatePlayback = null;
 
 // 2) On load, check whether the user is logged in
 ;(async () => {
