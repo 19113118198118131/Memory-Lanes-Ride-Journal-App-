9595e073d89a0a9fbc917f562cec2929232f20d8
19113118198118131 <samar.sharma@live.com>
2025-05-10 10:53:07 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Supabase config
-const SUPABASE_URL = 'https://vodujxiwkpxaxaqnwkdd.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZHVqeGl3a3B4YXhhcW53a2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NTgwOTQsImV4cCI6MjA2MjMzNDA5NH0.k4NeZ3dgqe1QQeXmkmgThp-X_PwOHPHLAQErg3hrPok'; // ANON key
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import supabase from './supabaseClient.js';
 
 console.log('script.js loaded');
 window.updatePlayback = null;
