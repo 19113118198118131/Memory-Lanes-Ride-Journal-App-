d02c186de2d2d0c38fa86093bea88417ea55c1db
19113118198118131 <samar.sharma@live.com>
2025-05-24 20:38:34 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1672,7 +1672,7 @@ addMomentBtn.addEventListener('click', () => {
   };
 
   // Main function to show fireworks for a set duration
-  window.showFireworks = function(duration = 2200) {
+  window.showFireworks = function(duration = 3000) {
     if (running) return;
     running = true;
     // Prepare canvas
